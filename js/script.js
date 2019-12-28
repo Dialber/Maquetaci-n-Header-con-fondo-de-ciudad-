b0ab24ad993a976819156977d04bc5713a480247
Dialber <dialberob@gmail.com>
2019-12-28 22:09:20 +0100

    me falta cambiar la sombra del monitor

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+    var imagen_telefono =document.getElementById('id-imagen-telefonos');
+
+    document.addEventListener('scroll',function(){	 
+        
+      var result=imagen_telefono.offsetTop.toString();
+           
+       })
+
+
+/* window.addEventListener('load',Trasladar_imagen,false); */
